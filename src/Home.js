10a5dadabd276b632489ef18ce0e761c42676979
swbrock <90523707+swbrock@users.noreply.github.com>
2023-04-18 09:49:25 -0600

    main

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import { Box, Button } from "@mui/material";
+import React from "react";
+
+export default function Home() {
+    return (
+        <Box>
+        <Button variant="contained">Hello World</Button>
+        </Box>
+    );
+}
+
